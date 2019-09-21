1d39ffd6712c4fa11ec1c97ad2bdb17ac5229bd3
srikamal <srikamal2001@gmail.com>
2019-09-21 14:33:40 -0400

    Rocket equation update

--- orbitalCalculations.py
+++ orbitalCalculations.py
@@ -24,13 +24,15 @@ def Rocket_Equation_Velocity(mass_of_payload, wet_mass, dry_mass, Specific_Impul
     total_mass = wet_mass + mass_of_payload
     # Ve of the RTsiolkovsky rocket equation can be calculated by multiplying
     # the specific impulse times the specific gravity of the earth
-    exhaust_velocity = Specific_Impulse*9.80665
+    exhaust_velocity = Specific_Impulse*STANDARD_GRAVITY
     # The log of the total wet mass to the dry mass will be calculated thus:
     log_of_wet_to_dry_mass = math.log(total_mass/dry_mass)
     # Thus the equation's delta - v can be calculated by multiplying log and the velocity
     delta_V = exhaust_velocity*log_of_wet_to_dry_mass
-    print("The delta-v is: ", delta_V, "m/s")
-    return delta_V
+    print("The delta-v needed to enter orbit is: ", delta_V, "m/s")
+    Energy_needed_for_orbit = (1/2)*total_mass*delta_V*delta_V
+    print("The magnitude of energy needed to enter orbit is : ", Energy_needed_for_orbit, "Joules")
+
 
 
 
